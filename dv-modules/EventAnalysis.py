2de923d98f46b5653dccb987c00c4cf98931f19b
Alphat18 <thomas.barbier@uca.fr>
2020-02-11 14:22:41 +0100

    Event statistics python module

--- dv-modules/EventAnalysis.py
+++ dv-modules/EventAnalysis.py
@@ -2,83 +2,146 @@ from dv import AedatFile
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import vstack, dok_matrix, csr_matrix
-from multiprocessing import Pool
+from numba import jit
+# import cv2
 
-tau = 1000
+tau = 50 # µs
+t_tau = 400000 # µs
+taus = np.arange(0, t_tau+1, tau)
+size = (346, 260) # px
+n = size[0] * size[1] # px
+l = 20 # px
+file_name = "/home/thomas/Vidéos/DVS_Recordings/air_hockey/air_hockey1.aedat4"
 
-def mat_corr(mat1: csr_matrix, mat2: csr_matrix, ind: int):
-    return correlation(mat1[:, ind], mat2[:, ind])
+nb_event = 1000000000
+freq_display = int(nb_event * 0.01) # %
 
-def correlation(mat1: csr_matrix, mat2: csr_matrix):
+def plot_spatial_correlation(spat_corr: np.array):
+    fig, axes = plt.subplots(2, 2, sharex=True, figsize=(10, 8))
+    fig.suptitle("Spatial correlation, \n probability of surrounding pixels firing given that a pixel will fire " + str(tau) + "µs later.", fontsize=16)
+    axes[0, 0].set_title("P(On | On)")
+    axes[0, 1].set_title("P(Off | On)")
+    axes[1, 0].set_title("P(On | Off)")
+    axes[1, 1].set_title("P(Off | Off)")
     
-    corr = np.zeros(tau)
-    for k in range(tau):
-        corr[k] = (vstack([mat1[k:], csr_matrix((k, 1))]).transpose() * mat2)[0, 0]
-    return corr
+    i = 0
+    for ax in axes.flat:
+        ax.imshow(spat_corr[i])
+        ax.set(xlabel='px', ylabel='px')
+        ax.label_outer()
+        i += 1
+    plt.show()
 
-if __name__ == "__main__":
-    with AedatFile("/home/thomas/Vidéos/DVS_Recordings/sport_30_min_video.aedat4") as f:
-        duration = 100000
-        size = (346, 260)
-        n = size[0]*size[1]
+def plot_temporal_correlation(temp_corr: np.array):
+    fig, axes = plt.subplots(4, figsize=(50, 30))
+    fig.suptitle("Temporal correlation, \n probability of a pixel firing given that this same pixel will fire in ranges of " + str(tau) + "µs.", fontsize=32)
+    axes[0].set_title("P(On | On)", fontsize=26)
+    axes[1].set_title("P(Off | On)", fontsize=26)
+    axes[2].set_title("P(On | Off)", fontsize=26)
+    axes[3].set_title("P(Off | Off)", fontsize=26)
+    
+    i = 0
+    for ax in axes.flat:
+        ax.plot(temp_corr[i])
+        ax.set_xticks(np.linspace(0, temp_corr[0].size, 101, dtype=np.int32))
+        ax.set_xticklabels(np.linspace(0, t_tau, 101, dtype=np.int32), rotation=45, fontsize=12)
+        ax.set(xlabel='delay (µs)', ylabel='probability (%)')
+        i += 1
+    plt.show()
+    
+def plot_temporal_cumsum(temp_corr: np.array):
+    cumsum_temp_corr = np.cumsum(temp_corr, axis=1)
+    fig, axes = plt.subplots(4, figsize=(50, 30))
+    fig.suptitle("Temporal correlation cumulative sum", fontsize=32)
+    axes[0].set_title("P(On | On)", fontsize=26)
+    axes[1].set_title("P(Off | On)", fontsize=26)
+    axes[2].set_title("P(On | Off)", fontsize=26)
+    axes[3].set_title("P(Off | Off)", fontsize=26)
+    
+    i = 0
+    for ax in axes.flat:
+        ax.plot(cumsum_temp_corr[i])
+        ax.set_xticks(np.linspace(0, temp_corr[0].size, 101, dtype=np.int32))
+        ax.set_xticklabels(np.linspace(0, t_tau, 101, dtype=np.int32), rotation=45, fontsize=12)
+        ax.set(xlabel='delay (µs)', ylabel='probability (%)')
+        i += 1
+    plt.show()
+    
+@jit(nopython=True)
+def spatial_correlation(x, y, polarity, timestamp, spat_corr, timestamps):
+    if x > l and x < size[0] - l and y > l and y < size[1] - l:
+        if polarity:
+            spat_corr[0] += 1 * (timestamps[1, y-l:y+l+1, x-l:x+l+1] > timestamp - tau) # P(On | On)
+            spat_corr[1] += 1 * (timestamps[0, y-l:y+l+1, x-l:x+l+1] > timestamp - tau) # P(Off | On)
+        else:
+            spat_corr[2] += 1 * (timestamps[1, y-l:y+l+1, x-l:x+l+1] > timestamp - tau) # P(On | Off)
+            spat_corr[3] += 1 * (timestamps[0, y-l:y+l+1, x-l:x+l+1] > timestamp - tau) # P(Off | Off)
+    timestamps[1*polarity, y, x] = timestamp
 
-        vecon = dok_matrix((duration, n), dtype=np.int8)
-        vecoff = dok_matrix((duration, n), dtype=np.int8)
-        # vecon = np.zeros(duration)
-        # vecoff = np.zeros(duration)
-        first = 0
-        lock = True
-        
-        for e in f['events']:
-            first = e.timestamp
-            break
-        
-        print("Spatial Correlation computation...")
-        mat = np.zeros((size[0], size[1]))
-        proba = np.zeros((size[0], size[1]))
-        for e in f['events']:
-            time = e.timestamp - first
-            if time > duration:
+@jit(nopython=True)
+def temporal_correlation(x, y, polarity, timestamp, temp_corr, timestamps):
+    if polarity:
+        for i, t in enumerate(taus):
+            temp_corr[0, i] += 1 * timestamps[0, y, x] >= timestamp - t and timestamps[0, y, x] < timestamp - t + tau # P(On | On)
+            temp_corr[1, i] += 1 * timestamps[1, y, x] >= timestamp - t and timestamps[1, y, x] < timestamp - t + tau # P(Off | On)
+    else:
+        for i, t in enumerate(taus):
+            temp_corr[2, i] += 1 * timestamps[0, y, x] >= timestamp - t and timestamps[0, y, x] < timestamp - t + tau # P(On | Off)
+            temp_corr[3, i] += 1 * timestamps[1, y, x] >= timestamp - t and timestamps[1, y, x] < timestamp - t + tau # P(Off | Off)
+    timestamps[1*polarity, y, x] = timestamp
+    
+
+#%% Spatial Correlation computation
+with AedatFile(file_name) as f:
+    print("Spatial Correlation computation...")
+    timestamps = np.zeros((2, size[1], size[0]))
+    spat_corr = np.zeros((4, 2*l+1, 2*l+1))
+    
+    count = 0
+    for e in f['events']:
+        if count % freq_display == 0:
+            print(100 * count / nb_event, "%")
+            np.save("spatial_correlation", spat_corr)
+            if count >= nb_event:
                 break
-        
-            mat[e.x, e.y] = e.timestamp
-            proba[e.x-1:e.x+2, e.y-1:e.y+2] += 1 * (mat[e.x-1:e.x+2, e.y-1:e.y+2] > e.timestamp - tau)
 
-#%%
-        print("Loading events...")
-        for e in f['events']:
-            time = e.timestamp - first
-            if time > duration:
+        spatial_correlation(e.x, e.y, e.polarity, e.timestamp, spat_corr, timestamps)
+        count += 1
+
+for i in range(4):
+    spat_corr[i] = spat_corr[i] / spat_corr[i].max()
+    
+#%% Plot
+plot_spatial_correlation(spat_corr)
+
+#%% Temporal Correlation computation
+with AedatFile(file_name) as f:
+    print("Temporal Correlation computation...")
+    timestamps = np.zeros((2, size[1], size[0]))
+    temp_corr = np.zeros((4, taus.size))
+    
+    count = 0
+    for e in f['events']:
+        if count % freq_display == 0:
+            print(100 * count / nb_event, "%")
+            np.save("temporal_correlation", temp_corr)
+            if count >= nb_event:
                 break
+    
+        temporal_correlation(e.x, e.y, e.polarity, e.timestamp, temp_corr, timestamps)
+        count += 1
+    
+temp_corr = temp_corr / count
 
-            if e.polarity:
-                vecon[time, e.x*size[1] + e.y] = 1
-            else:
-                vecoff[time, e.x*size[1] + e.y] = 1
+#%% Plot
+plot_temporal_correlation(100*temp_corr)
 
-#%%
-        print("Spatial Correlation computation...")
-        mat = np.zeros((size[0], size[1]))
-        proba = np.zeros((size[0], size[1]))
-        for e in f['events']:
-            time = e.timestamp - first
-            if time > duration:
-                break
-        
-            mat[e.x, e.y] = e.timestamp
-            proba[e.x-1:e.x+2, e.y-1:e.y+2] += 1 * (mat[e.x-1:e.x+2, e.y-1:e.y+2] > e.timestamp - tau)
+#%% Plot
+plot_temporal_cumsum(100*temp_corr)
 
-#%%
-        print("Temporal Correlation computation...")
-        nb_proc = 8
-        
-        args_oo = [(vecon, vecon, i) for i in range(8)]        
-        
-        with Pool(nb_proc) as p:
-            corr_oo = np.sum(p.starmap(mat_corr, args_oo), axis=0)
-
-#%%
-        print("Display")
-        plt.plot(corr_oo)
-        plt.show()
+#%% Loop frames
+# with AedatFile(file_name) as f:    
+#     for frame in f['frames']:
+#         cv2.imshow('out', frame.image)
+#         cv2.waitKey(100)
+#     cv2.destroyAllWindows()
