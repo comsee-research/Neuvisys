aaa0d2d9f0871c44c459cac50e63375d0fd914d8
Alphat18 <firetz810@gmail.com>
2021-05-06 12:30:44 +0200

    solving issues with NetworkHandle

--- src/modules/Neuvisys.cpp
+++ src/modules/Neuvisys.cpp
@@ -2,15 +2,13 @@
 
 int main(int argc, char *argv[]) {
     if (argc > 2) {
-        auto neuvisys = NetworkHandle(argv[1], argv[2], static_cast<size_t>(std::stoi(argv[3])));
-        neuvisys.multiplePass();
+        multiplePass(argv[1], argv[2], static_cast<size_t>(std::stoi(argv[3])));
     } else {
         std::cout << "too few arguments, entering debug mode" << std::endl;
 
         std::string networkPath = "/home/alphat/Desktop/Networks/network_0/configs/network_config.json";
         std::string events = "/home/alphat/Desktop/shapes.npz";
 
-        auto neuvisys = NetworkHandle(networkPath, events, 1);
-        neuvisys.multiplePass();
+        multiplePass(networkPath, events, 1);
     }
 }
