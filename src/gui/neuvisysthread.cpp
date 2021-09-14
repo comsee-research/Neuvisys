6f2173563d2baf0bde0b3fe0513e1938dc7673e6
Alphat18 <thomas.barbier@uca.fr>
2021-09-15 00:40:27 +0200

    works with v dot

--- src/gui/neuvisysthread.cpp
+++ src/gui/neuvisysthread.cpp
@@ -110,21 +110,20 @@ void NeuvisysThread::rosPass(SpikingNetwork &spinet) {
             spinet.updateTDError(sim.getLeftEvents().back().timestamp());
         }
 
-//        if (sim.getSimulationTime() - actionTime > m_actionRate / 1e6) {
-//            actionTime = sim.getSimulationTime();
-//            if (m_actor != -1) {
-//                auto neuron = spinet.getNeuron(m_actor, spinet.getNetworkStructure().size() - 1);
-//                neuron.get().spike(sim.getLeftEvents().back().timestamp());
-//                neuron.get().setNeuromodulator(spinet.updateTDError());
-//                neuron.get().weightUpdate();
-//            }
-//            sim.motorAction(spinet.resolveMotor(), 0, m_actor);
-//            m_value = spinet.getRewards().back();
-//
-//            if (m_actor != -1) {
-//                m_motorDisplay[m_actor] = true;
-//            }
-//        }
+        if (sim.getSimulationTime() - actionTime > m_actionRate / 1e6) {
+            actionTime = sim.getSimulationTime();
+            if (m_actor != -1) {
+                auto neuron = spinet.getNeuron(m_actor, spinet.getNetworkStructure().size() - 1);
+                neuron.get().spike(sim.getLeftEvents().back().timestamp());
+                neuron.get().setNeuromodulator(spinet.updateTDError());
+                neuron.get().weightUpdate();
+            }
+            sim.motorAction(spinet.resolveMotor(), 0, m_actor);
+
+            if (m_actor != -1) {
+                m_motorDisplay[m_actor] = true;
+            }
+        }
 
         if (sim.getSimulationTime() - displayTime > m_displayRate / Conf::E6) {
             displayTime = sim.getSimulationTime();
