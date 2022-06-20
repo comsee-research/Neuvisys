49a73aa62cb30a31177fd492d9ff0c042a135ca8
Alphat18 <thomas.barbier@uca.fr>
2022-06-20 13:09:09 +0200

    simulator

--- src/gui/Gui.cpp
+++ src/gui/Gui.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     NeuvisysGUI w(argc, argv,
                   "/home/thomas/Videos/natural/shapes/shapes_multiple_disparity.h5",
                   "/home/thomas/Networks/simulation/rl/orientation_task/simple_only/network_learning/");
-    w.setFixedSize(0.9 * QDesktopWidget().availableGeometry().size());
+    w.setFixedSize(0.95 * QDesktopWidget().availableGeometry().size());
     w.show();
     return app.exec();
 }
