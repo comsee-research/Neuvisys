57d244856b7c5c230f43b1101bc0f16c370449ee
Alphat18 <thomas.barbier@uca.fr>
2022-06-24 14:54:34 +0200

    merge

--- src/gui/Gui.cpp
+++ src/gui/Gui.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     NeuvisysGUI w(argc, argv,
-                  "/home/thomas/Videos/natural/shapes/shapes_multiple_disparity.h5",
-                  "/home/thomas/Networks/simulation/rl/orientation_task/simple_only/network_learning/");
+                  "/home/thomas/Videos/natural/shapes/shapes.h5",
+                  "/home/thomas/Bureau/test4/");
     w.setFixedSize(0.95 * QDesktopWidget().availableGeometry().size());
     w.show();
     return app.exec();
