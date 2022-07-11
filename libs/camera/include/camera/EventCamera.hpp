76c01e023a6800bf23a01136e410f9dc4473730d
Mathieu Labussiere <mathieu.labu@gmail.com>
2022-07-11 15:45:41 +0200

    [refacto] add camera library

---
+++ libs/camera/include/camera/EventCamera.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Thomas on 23/03/2022.
+//
+
+#ifndef NEUVISYS_EVENTCAMERA_HPP
+#define NEUVISYS_EVENTCAMERA_HPP
+
+#include <atomic>
+#include <libcaercpp/devices/davis.hpp>
+#include <csignal>
+
+class EventCamera {
+    libcaer::devices::davis davis = libcaer::devices::davis(1);
+
+public:
+    EventCamera();
+
+    ~EventCamera();
+
+    void changeBiases();
+
+    std::shared_ptr<const libcaer::events::PolarityEventPacket> receiveEvents(bool &received, bool &stop);
+
+private:
+    static int prepareContext();
+};
+
+#endif //NEUVISYS_EVENTCAMERA_HPP
